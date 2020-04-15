e8a47d68a274312075b3c56a5eb8b35ba56739b8
Brent Cralley <brentcralley@gmail.com>
2020-04-15 13:43:31 -0500

    .env and .gitignore updates

--- server.js
+++ server.js
@@ -4,21 +4,30 @@ const cors = require('cors');
 const router = require('./routes/index');
 
 const app = express(); 
-const PORT = 3001; 
-const MONGODB_URI = "mongodb+srv://brentcralley:pass@portfolio-i60wi.gcp.mongodb.net/test?retryWrites=true&w=majority"; 
+const path = require('path');           
+const PORT = process.env.PORT || 3001;  
+require('dotenv').config(); 
 
 app.use(cors())
 app.use(express.urlencoded({ extended: true })); 
 app.use(express.json());
 app.use('/api', router); 
 
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useFindAndModify: false }); 
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useFindAndModify: false }); 
 mongoose.connection.once('open', function() { 
   console.log('Connected to the Database.');
 });
 mongoose.connection.on('error', function(error) {
   console.log('Mongoose Connection Error : ' + error);
 });
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(_dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 app.listen(PORT, function() { 
   console.log(`Server listening on port ${PORT}.`);
